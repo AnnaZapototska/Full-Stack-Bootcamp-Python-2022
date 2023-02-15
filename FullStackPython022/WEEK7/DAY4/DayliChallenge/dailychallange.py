35581265a42819c1cee67359407e3ec404968316
AnnaZapototska <zapototskaann@gmail.com>
2023-02-15 21:34:31 +0200

    add

--- FullStackPython022/WEEK7/DAY4/DayliChallenge/dailychallange.py
+++ FullStackPython022/WEEK7/DAY4/DayliChallenge/dailychallange.py
@@ -1,9 +1,16 @@
 # Write a function to compute 5/0 and use try/except to catch the exceptions.
 # Bonus : use some Buit-in exceptions of Python : Look here
-try:
-    result = 5 /0
-except ZeroDivisionError:
-    print("Error: division by zero")
+#  solution 1
+# try:
+#     result = 5 /0
+# except ZeroDivisionError:
+#     print("Error: division by zero")
 
-
-
+# solution 2 with function
+def division_zero():
+    try:
+        result = 5 /0
+    except ZeroDivisionError:
+        print("Error: division by zero")
+        
+division_zero()
