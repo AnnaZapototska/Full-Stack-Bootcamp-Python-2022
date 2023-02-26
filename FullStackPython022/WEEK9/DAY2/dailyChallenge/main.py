fb5efab0d2a3a5dfec2d2ae2bf8bc869d195ecb7
AnnaZapototska <zapototskaann@gmail.com>
2023-02-26 22:03:35 +0200

    clean code

---
+++ FullStackPython022/WEEK9/DAY2/dailyChallenge/main.py
@@ -0,0 +1,67 @@
+import math
+
+
+class Circle:
+    def __init__(self, radius=None, diameter=None):
+        if radius is not None and diameter is not None:
+            raise ValueError("Both radius and diameter cannot be specified")
+        elif radius is not None:
+            self.radius = radius
+            self.diameter = radius * 2
+        elif diameter is not None:
+            self.diameter = diameter
+            self.radius = diameter / 2
+        else:
+            raise ValueError("Either radius or diameter must be specified")
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def draw(self):
+        size = self.radius * 2 + 1
+        grid = [[' ' for _ in range(size)] for _ in range(size)]
+        for y in range(size):
+            for x in range(size):
+                dx = x - self.radius
+                dy = y - self.radius
+                distance = math.sqrt(dx ** 2 + dy ** 2)
+                if distance <= self.radius:
+                    grid[y][x] = '*'
+
+        # Print the grid
+        for row in grid:
+            print(' '.join(row))
+
+    def __str__(self):
+        return f"Circle with radius {self.radius}"
+
+    def __repr__(self):
+        return f"Circle({self.radius})"
+
+    def __add__(self, other):
+        print("The radius equal to the sum of the radii of the two circles")
+        return print(Circle(radius=self.radius + other.radius))
+
+    def __lt__(self, other):
+        print("Compares two circles to see which is smaller")
+        return print(self.radius < other.radius)
+
+    def __eq__(self, other):
+        print("Compares two circles to see if they are equal")
+        return print(self.radius == other.radius)
+
+c = Circle(radius=2)
+print(f"The area circle: {c.area()}")
+c.draw()
+
+c1 = Circle(radius=8)
+print(f"The area circle: {c1.area()}")
+c1.draw()
+
+c2 = Circle(radius=8)
+print(f"The area circle: {c2.area()}")
+c2.draw()
+
+list_circles = [c, c1, c2]
+list_circles.sort()
+print(list_circles)
