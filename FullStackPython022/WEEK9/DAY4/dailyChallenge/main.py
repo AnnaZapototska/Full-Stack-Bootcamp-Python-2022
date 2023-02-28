590fe89a92d3cda6f62bddc3ac87bafc0cc94f56
AnnaZapototska <zapototskaann@gmail.com>
2023-02-28 20:52:41 +0200

    clean code

---
+++ FullStackPython022/WEEK9/DAY4/dailyChallenge/main.py
@@ -0,0 +1,20 @@
+#
+class Person:
+    def __init__(self, name, likes, hates):
+        self.name = name
+        self.likes = likes
+        self.hates = hates
+
+    def taste(self, food_name):
+        if food_name in self.likes:
+            return f"{self.name} eats the {food_name} and loves it!"
+        elif food_name in self.hates:
+            return f"{self.name} eats the {food_name} and hates it!"
+        else:
+            return f"{self.name} eats the {food_name}!"
+
+
+person1 = Person("Anna", ["cake"], ["buckwheat"])
+print(person1.taste("cake"))
+print(person1.taste("cheese"))
+print(person1.taste("carrots"))
