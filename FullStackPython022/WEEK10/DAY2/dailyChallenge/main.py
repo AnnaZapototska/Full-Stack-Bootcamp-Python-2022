4d9787e6343bcdaf097ffb4a278062e39567a95f
AnnaZapototska <zapototskaann@gmail.com>
2023-03-04 23:27:17 +0200

    clean code

---
+++ FullStackPython022/WEEK10/DAY2/dailyChallenge/main.py
@@ -0,0 +1,15 @@
+#
+from translate import Translator
+
+french_words = ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"]
+translations = {}
+
+# Loop through each French word and translate it to German
+for word in french_words:
+    translator = Translator(to_lang="English")
+    translation = translator.translate(word)
+    translations[word] = translation
+
+# Print the resulting dictionary
+print(translations)
+
